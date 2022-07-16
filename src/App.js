c66ab77111f4426e022bc28e991df8ebf935f9b1
ricsign <ricsign123@gmail.com>
2022-07-16 17:59:15 -0600

    initial connection established

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    alanBtn({
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: ({command, recipes}) => {
+        // eslint-disable-next-line default-case
+        switch (command) {
+          case "newRecipes":
+            console.log(recipes);
+            console.log(recipes.count);
+            break;
+        }
+      }
+    })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Acoustic Recipe</h1>
     </div>
   );
 }
